2af7d12b3753292432ccd35bc7f320e0c5fc7747
Gabriel Fleig Alves <fleigalves@hotmail.com>
2023-09-26 11:11:39 -0300

    navegação de páginas e sidebar show
    testar sem conteúdo nos docs para ver se matemática é firme

--- js/controller.js
+++ js/controller.js
@@ -2,8 +2,16 @@ document.addEventListener('DOMContentLoaded', function () {
     var menuNav = document.getElementById('menu-nav');
     var sideBar = document.getElementById('sidebar-menu');
     var cabecalho = document.getElementById('cabecalho-id');
+    var corpo = document.getElementById('conteudo');
+    var sidebar = document.getElementById('sidebar-menu');
     var offsetValue = cabecalho.offsetHeight;
     var menuHeight = menuNav.offsetHeight;
+    var corpoHeight = corpo.offsetHeight;
+    var sidebarHeight = sidebar.offsetHeight;
+
+    console.log('cabecalho height:' + offsetValue);
+    console.log('corpo height:' + corpoHeight);
+    console.log('sidebar height:' + sidebarHeight);
     
     window.addEventListener('scroll', function () {
         // Use console.log to determine when you want the nav bar to stick.
@@ -17,10 +25,21 @@ document.addEventListener('DOMContentLoaded', function () {
             cabecalho.style.height = offsetValue + 'px';
         }
 
-        if (window.pageYOffset >= (offsetValue)) {
+        if (window.pageYOffset >= (offsetValue) && (window.pageYOffset < (offsetValue + corpoHeight - sidebarHeight))) {
             sideBar.classList.add('sidebar-fixed');
+            sideBar.classList.remove('sidebar-wrapper');
+            sideBar.classList.remove('sidebar-end');
+            console.log('fixed');
+        } else if (window.pageYOffset >= (offsetValue + corpoHeight - sidebarHeight)){
+            sideBar.classList.remove('sidebar-fixed');
+            sideBar.classList.remove('sidebar-wrapper');
+            sideBar.classList.add('sidebar-end');
+            console.log('end');
         } else {
             sideBar.classList.remove('sidebar-fixed');
+            sideBar.classList.remove('sidebar-end');
+            sideBar.classList.add('sidebar-wrapper');
+            console.log('nada');
         }
     });
 });
