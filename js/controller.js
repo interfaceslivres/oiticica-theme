528f2f2117953bd2abad8e75c82a1ede50edc44e
Gabriel Fleig Alves <fleigalves@hotmail.com>
2023-11-21 12:10:47 -0300

    hamburger indo, ajeitar scroll

--- js/controller.js
+++ js/controller.js
@@ -1,6 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
 
+    var overlay = document.getElementById('menu-overlay');
+    var corpo_doc= document.getElementById('documentos');
+
     var searchButton = document.getElementById('busca');
+    var menuButton = document.getElementById('hamburger')
     var closeSearchButton = document.getElementById('busca-fecha');
     var searchBar = document.getElementById('busca-barra');
     var menuBar = document.getElementById('desktop-menu');
@@ -9,14 +13,20 @@ document.addEventListener('DOMContentLoaded', function () {
 
     searchButton.addEventListener('click', buscaClick);
     closeSearchButton.addEventListener('click', buscaClick);
+    menuButton.addEventListener('click', toggleMenuOverlay);
 
     function buscaClick() {
         searchBar.classList.toggle('hidden');
         menuBar.classList.toggle('hidden');
-        menuButtons.classList.toggle('hidden');
+        menuButtons.classList.toggle('hidden');        
     }
 
-    var menuNav = document.getElementById('menu-nav');
+    function toggleMenuOverlay() {
+        overlay.classList.toggle('hidden');
+        document.body.classList.toggle('stop-scrolling');
+    }
+
+    var menuNav = document.getElementById('menu-nav');    
     var sideBar = document.getElementById('sidebar-menu');
     var cabecalho = document.getElementById('cabecalho-id');
     var corpo = document.getElementById('conteudo');
@@ -25,6 +35,8 @@ document.addEventListener('DOMContentLoaded', function () {
     var menuHeight = menuNav.offsetHeight;
     var corpoHeight = corpo.offsetHeight;
     var sidebarHeight = sidebar.offsetHeight;
+
+    overlay.style.top = offsetValue + menuHeight - window.pageYOffset + 'px';
 
     /*console.log('cabecalho height:' + offsetValue);
     console.log('menu height:' + menuHeight);
@@ -35,12 +47,16 @@ document.addEventListener('DOMContentLoaded', function () {
         // Use console.log to determine when you want the nav bar to stick.
         //console.log(window.pageYOffset);
         
+        
+
         if (window.pageYOffset >= offsetValue) {
             menuNav.classList.add('top-fixed');
             cabecalho.style.height = offsetValue + menuHeight + 'px';
+            overlay.style.top = menuHeight + 'px';
         } else {
             menuNav.classList.remove('top-fixed');
             cabecalho.style.height = offsetValue + 'px';
+            overlay.style.top = offsetValue + menuHeight - window.pageYOffset + 'px';
         }
 
         if (window.pageYOffset >= (offsetValue) && (window.pageYOffset < (offsetValue + corpoHeight - sidebarHeight))) {
